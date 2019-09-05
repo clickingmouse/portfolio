631988e3fa69068b49f33340f33e584f36896246
clickingmouse <clickingmouse@users.noreply.github.com>
2019-09-05 21:31:04 +0800

    deploy heroku

--- client/src/components/List.js
+++ client/src/components/List.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { Container, ListGroup, ListGroupItem, Button } from "reactstrap";
 import { CSSTransition, TransitionGroup } from "react-transition-group";
-import uuid from "uuid";
+//import uuid from "uuid";
 import { connect } from "react-redux";
 import { getItems, deleteItem } from "../actions/itemActions";
 import PropTypes from "prop-types";
--- server.js
+++ server.js
@@ -1,15 +1,20 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-
+const path = require("path");
 const app = express();
 const items = require("./routes/api/items");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
 //Body parser middleware
 app.use(bodyParser.json());
 
 //DB Config
-const db = require("./config/keys").mongoURI;
-
+//const db = require("./config/keys").mongoURI;
+//console.log(process.env.REACT_APP_MONGO_URI);
+const db = process.env.REACT_APP_MONGO_URI;
 //connect to mongoose
 mongoose
   .connect(db)
@@ -17,6 +22,15 @@ mongoose
   .catch(err => console.log(err));
 
 app.use("/api/items", items);
+
+// serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const port = process.env.PORT || 5000;
 
