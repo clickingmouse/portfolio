8f224a9511c4bc9bf0bc527d710fe84746f5aed9
clickingmouse <clickingmouse@users.noreply.github.com>
2019-09-05 11:07:53 +0800

    item schema created

--- server.js
+++ server.js
@@ -9,3 +9,13 @@ app.use(bodyParser.json());
 
 //DB Config
 const db = require("./config/keys").mongoURI;
+
+//connect to mongoose
+mongoose
+  .connect(db)
+  .then(() => console.log("mongo connected..."))
+  .catch(err => console.log(err));
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`server started on ${port}`));
