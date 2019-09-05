26d4d1405b376318384fde568a24b78e75a84eb1
clickingmouse <clickingmouse@users.noreply.github.com>
2019-09-05 15:05:01 +0800

    added list

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import AppNavBar from "./components/AppNavBar";
+import List from "./components/List";
 function App() {
   return (
     <div className="App">
       <AppNavBar />
       <h1>Hello</h1>
+      <List />
     </div>
   );
 }
