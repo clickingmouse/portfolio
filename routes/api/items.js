4f590515457a8cbf8a6723162bc328302a23cf26
clickingmouse <clickingmouse@users.noreply.github.com>
2019-09-05 12:14:47 +0800

    added delete route

--- routes/api/items.js
+++ routes/api/items.js
@@ -23,4 +23,17 @@ router.post("/", (req, res) => {
   newItem.save().then(item => res.json(item));
 });
 
+// @route DELETE api/item/:id
+// @descr delete an item
+// @access Public
+router.delete("/:id", (req, res) => {
+  Item.findById(req.params.id)
+    .then(item => item.remove().then(() => res.json({ success: true })))
+    .catch(err =>
+      res.status(404).json({
+        success: false
+      })
+    );
+});
+
 module.exports = router;
